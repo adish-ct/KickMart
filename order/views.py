309c51a8d1d2d461f085b32e9b139d073346adc9
adish-ct <adishathu525@gmail.com>
2023-09-17 00:38:12 +0530

    order confirmed view completed

--- order/views.py
+++ order/views.py
@@ -23,16 +23,20 @@ from xhtml2pdf import pisa
 @cache_control(no_cache=True, no_store=True)
 @login_required(login_url='index')
 def order(request):
-    user = request.user
-    checkout_items = Checkout.objects.get(user=user)
-    cart_items = CartItem.objects.filter(customer=user)
-    if not cart_items:
-        return redirect('index')
+    context = {}
+    try:
+        user = request.user
+        checkout_items = Checkout.objects.get(user=user)
+        cart_items = CartItem.objects.filter(customer=user)
+        if not cart_items:
+            return redirect('index')
 
-    context = {
-        'checkout_items': checkout_items,
-        'cart_items': cart_items,
-    }
+        context = {
+            'checkout_items': checkout_items,
+            'cart_items': cart_items,
+        }
+    except Exception as e:
+        print(e)
     return render(request, 'product/order_confirm.html', context)
 
 
@@ -45,90 +49,93 @@ def order_confirmed(request):
         cart_items = CartItem.objects.filter(customer=my_user)
     except:
         return redirect('index')
-    checkout = Checkout.objects.get(user=my_user)
-    if request.method == 'POST':
-        total = checkout.grand_total
-        my_order = Order()
-        my_order.user = my_user
-        my_order.address = checkout.address
-        my_order.paid_amount = checkout.payable_amount
-        my_order.total = checkout.total  # product's total amount.
-        my_order.order_total = checkout.grand_total  # total amount including tax and other charges.
-        my_order.discount = checkout.discount
-        my_order.wallet_amount = checkout.wallet
-        my_order.tax = checkout.tax
-        my_order.ip = request.META.get('REMOTE_ADDR')
-        my_order.is_ordered = True
-        my_order.coupon = checkout.coupon
+    try:
+        checkout = Checkout.objects.get(user=my_user)
+        if request.method == 'POST':
+            total = checkout.grand_total
+            my_order = Order()
+            my_order.user = my_user
+            my_order.address = checkout.address
+            my_order.paid_amount = checkout.payable_amount
+            my_order.total = checkout.total  # product's total amount.
+            my_order.order_total = checkout.grand_total  # total amount including tax and other charges.
+            my_order.discount = checkout.discount
+            my_order.wallet_amount = checkout.wallet
+            my_order.tax = checkout.tax
+            my_order.ip = request.META.get('REMOTE_ADDR')
+            my_order.is_ordered = True
+            my_order.coupon = checkout.coupon
 
-        if float(checkout.grand_total) < float(checkout.wallet):
-            my_user.wallet = float(checkout.wallet) - float(checkout.grand_total)
-        else:
-            my_user.wallet = 0
+            if float(checkout.grand_total) < float(checkout.wallet):
+                my_user.wallet = float(checkout.wallet) - float(checkout.grand_total)
+            else:
+                my_user.wallet = 0
 
-        my_user.save()
-        my_order.save()
+            my_user.save()
+            my_order.save()
 
-        # creating order with current date and order id
-        yr = int(datetime.date.today().strftime('%Y'))
-        dt = int(datetime.date.today().strftime('%d'))
-        mt = int(datetime.date.today().strftime('%m'))
-        d = datetime.date(yr, mt, dt)
-        current_date = d.strftime("%Y%m%d")
-        order_id = current_date + str(my_order.id)  # creating order id
-        my_order.order_id = order_id
+            # creating order with current date and order id
+            yr = int(datetime.date.today().strftime('%Y'))
+            dt = int(datetime.date.today().strftime('%d'))
+            mt = int(datetime.date.today().strftime('%m'))
+            d = datetime.date(yr, mt, dt)
+            current_date = d.strftime("%Y%m%d")
+            order_id = current_date + str(my_order.id)  # creating order id
+            my_order.order_id = order_id
 
-        my_order.save()
+            my_order.save()
 
-        # creating object for payment.
-        payment_type = request.POST.get('paymentMode')
-        payment = Payments.objects.create(
-            user=my_user,
-            total_amount=checkout.grand_total,
-            is_paid=False,
-        )
-
-        # creating order items
-        cart_items = CartItem.objects.filter(customer=my_user)
-        for item in cart_items:
-            variant = ProductVariant.objects.get(id=item.product.id)
-            order_item = OrderProduct.objects.create(
-                customer=my_user,
-                order_id=my_order,
-                payment_id=payment.id,
-                variant=variant,
-                quandity=item.quantity,
-                product_price=item.product.product_price,
-                ordered=True,
+            # creating object for payment.
+            payment_type = request.POST.get('paymentMode')
+            payment = Payments.objects.create(
+                user=my_user,
+                total_amount=checkout.grand_total,
+                is_paid=False,
             )
-            variant.stock = variant.stock - item.quantity
-            variant.save()
-            item.delete()
-        try:
-            cart_items.delete()
-        except:
-            pass
 
-        # payment method : cashon delivery
-        if payment_type == 'cashonDelivery':
-            payment.payment_method = 'Cashon Delivery'  # set current payment method
-            payment_id = order_id + "COD"
-            payment.payment_id = payment_id
+            # creating order items
+            cart_items = CartItem.objects.filter(customer=my_user)
+            for item in cart_items:
+                variant = ProductVariant.objects.get(id=item.product.id)
+                order_item = OrderProduct.objects.create(
+                    customer=my_user,
+                    order_id=my_order,
+                    payment_id=payment.id,
+                    variant=variant,
+                    quandity=item.quantity,
+                    product_price=item.product.product_price,
+                    ordered=True,
+                )
+                variant.stock = variant.stock - item.quantity
+                variant.save()
+                item.delete()
+            try:
+                cart_items.delete()
+            except:
+                pass
+
+            # payment method : cashon delivery
+            if payment_type == 'cashonDelivery':
+                payment.payment_method = 'Cashon Delivery'  # set current payment method
+                payment_id = order_id + "COD"
+                payment.payment_id = payment_id
+                payment.save()
+                my_order.payment = payment
+                my_order.save()
+                return render(request, 'product/order_confirmed.html')
+
+            payment.payment_method = 'Razor Pay'  # set current payment method
+            payment.payment_id = request.POST.get('payment_id')  # check this payment_id
+            payment.is_paid = True
             payment.save()
             my_order.payment = payment
             my_order.save()
-            return render(request, 'product/order_confirmed.html')
 
-        payment.payment_method = 'Razor Pay'  # set current payment method
-        payment.payment_id = request.POST.get('payment_id')  # check this payment_id
-        payment.is_paid = True
-        payment.save()
-        my_order.payment = payment
-        my_order.save()
-
-        return JsonResponse({
-            'status': True,
-        })
+            return JsonResponse({
+                'status': True,
+            })
+    except Exception as e:
+        print(e)
 
     return render(request, 'product/order_confirmed.html')
 
@@ -179,13 +186,13 @@ def order_cancel(requset, order_id):
             if reason:
                 order_product.return_reason = reason
 
-            order.status = "Cancelled"
-            payment.status = 'Order cancelled'
+            # order.status = "Cancelled"
+            # payment.status = 'Order cancelled'
             order_product.item_cancel = True
             variant.stock += order_product.quandity
 
             if payment.payment_method != "Cashon Delivery":
-                # cash back logic should be handle here.
+                # cash back logic should be handled here.
 
                 if order.total < 2500:  # delivery charge logic.
                     delivery_charge = 99
@@ -285,7 +292,7 @@ class ViewPDF(View):
         return HttpResponse(pdf, content_type='application/pdf')
 
 
-# Automaticly downloads to PDF file
+# Automatically downloads to PDF file
 class DownloadPDF(View):
     def get(self, request, order_id, *args, **kwargs):
         user = request.user
