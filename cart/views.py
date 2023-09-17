27f4a31ebda194f28b7c370bf60c478870f1b544
adish-ct <adishathu525@gmail.com>
2023-09-17 17:11:22 +0530

    cart quandity update

--- cart/views.py
+++ cart/views.py
@@ -52,7 +52,8 @@ def cart(request, quantity=0, total=0, cart_items=None, tax=0, grand_total=0, co
             tax = (total * 3) // 100
             checkout.tax = tax  # checkout saving tax
 
-            # after submiting the coupon form. Coupon handling logic
+            # after submitting the coupon form. Coupon handling logic
+
             if request.method == 'POST':
                 arr = []  # for storing used coupon
                 orders = Order.objects.filter(user=checkout_user)
@@ -274,101 +275,25 @@ def add_cart(request, product_id):
 @cache_control(no_cache=True, no_store=True)
 def remove_cart_quandity(request):
     if request.method == 'POST':
-        grant_total = 0
+        grant_total, total_amount, total, tax = 0, 0, 0, 0
         delivery_charge = 99
-        total_amount = 0
+
         variant_id = request.POST.get('product_id')
         product_quantity = request.POST.get('product_quandity')
-        variant = ProductVariant.objects.get(id=variant_id)
+        product_stock = request.POST.get('productStock')
 
-        if 'user' in request.session:
-            my_user = request.user.id
-            try:
-                cart_item = CartItem.objects.get(product=variant, customer=my_user)
-                if cart_item.quantity > 1:
-                    cart_item.quantity -= 1
-                    cart_item.save()
-
-                else:
-                    cart_item.delete()
-
-                product_quantity = cart_item.quantity
-                total = cart_item.product.product_price * cart_item.quantity
-                if total <= 0:
-                    delivery_charge = 0
-
-                cart_items = CartItem.objects.filter(customer=my_user).order_by('id')
-                for item in cart_items:
-                    grant_total += (item.product.product_price * item.quantity)
-
-            except CartItem.DoesNotExist:
-                pass
-
-            try:
-                # check out handling
-                checkout = Checkout.objects.get(user=my_user)
-
-                checkout.total = grant_total
-                if grant_total > 2500:
-                    delivery_charge = 0
-                checkout.shipping = delivery_charge  # checkout saving delivery charge
-
-                tax = (grant_total * 3) // 100
-                checkout.tax = tax
-                total_amount = (grant_total + tax + delivery_charge)  # calculating grand total.
-                checkout.grand_total = total_amount
-
-                checkout.save()
-            except Checkout.DoesNotExist:
-                pass
-
-        else:
-            cart = Cart.objects.get(
-                cart_id=_cart_id(request))  # fetch the current cart, cart will be present in this case.
-            try:
-                cart_item = CartItem.objects.get(product=variant, cart=cart)  # fetch the cart item
-                if cart_item.quantity > 1:
-                    cart_item.quantity = cart_item.quantity - 1
-                    cart_item.save()
-                else:
-                    cart_item.delete()
-                product_quantity = cart_item.quantity
-            except CartItem.DoesNotExist:
-                pass
-
-            return redirect('cart')
-
-        return JsonResponse({
-            'quantity': product_quantity,
-            'total': total,
-            'delivery_charge': delivery_charge,
-            'grant_total': grant_total,
-            'total_amount': checkout.grand_total,
-            'tax': checkout.tax,
-        })
-    else:
-        return redirect('cart')
-
-
-@cache_control(no_cache=True, no_store=True)
-def add_cart_quandity(request):
-    if request.method == 'POST':
-        grant_total = 0
-        delivery_charge = 99
-        total_amount = 0
-        variant_id = request.POST.get('product_id')
-        product_quantity = request.POST.get('product_quandity')
         variant = ProductVariant.objects.get(id=variant_id)
 
         if 'user' in request.session:
             my_user = request.user
-
+            print("---------------------------")
+            print(my_user)
             try:
                 cart_item = CartItem.objects.get(customer=my_user, product=variant)
-                if variant.stock > cart_item.quantity:
-                    cart_item.quantity += 1
-                else:
-                    messages.error(request, "No more products available in the current variant")
+                print("---------------------------")
+                print(variant)
+                if cart_item.quantity > 1:
+                    cart_item.quantity -= 1
 
                 cart_item.save()
 
@@ -390,17 +315,18 @@ def add_cart_quandity(request):
                 if grant_total > 2500:
                     delivery_charge = 0
                 checkout.shipping = delivery_charge  # checkout saving delivery charge
-
-                tax = (grant_total * 3) // 100
+                tax = (grant_total * 3) // 100  # tax logic
                 checkout.tax = tax
                 total_amount = (grant_total + tax + delivery_charge)  # calculating grand total.
                 checkout.grand_total = total_amount
 
                 checkout.save()
+
             except Checkout.DoesNotExist:
                 pass
 
         else:
+
             try:
                 cart = Cart.objects.get(cart_id=_cart_id(request))
                 cart_item = CartItem.objects.get(cart=cart, product=variant)
@@ -412,15 +338,180 @@ def add_cart_quandity(request):
                 cart_item.save()
                 total = cart_item.product.product_price * cart_item.quantity
 
-
             except CartItem.DoesNotExist:
                 pass
+
         return JsonResponse({
             'quantity': product_quantity,
             'total': total,
             'grant_total': grant_total,
-            'total_amount': checkout.grand_total,
-            'tax': checkout.tax,
+            'tax': tax,
+            'total_amount': total_amount,
+            'delivery_charge': delivery_charge,
+        })
+    else:
+        return redirect('cart')
+
+
+# @cache_control(no_cache=True, no_store=True)
+# def remove_cart_quandity(request):
+#     # ajax request
+#     if request.method == 'POST':
+#         grant_total, total_amount, total, tax = 0, 0, 0, 0
+#         delivery_charge = 99
+#
+#         variant_id = request.POST.get('product_id')
+#         product_quantity = request.POST.get('product_quandity')
+#         product_stock = request.POST.get('product_stock')
+#
+#         variant = ProductVariant.objects.get(id=variant_id)
+#
+#         if 'user' in request.session:
+#             my_user = request.user.id
+#             try:
+#                 cart_item = CartItem.objects.get(product=variant, customer=my_user)
+#
+#                 if cart_item.quantity > 1:
+#                     cart_item.quantity -= 1
+#                 cart_item.save()
+#                 # deleted else case
+#
+#                 # single product total is total
+#                 product_quantity = cart_item.quantity
+#                 total = cart_item.product.product_price * cart_item.quantity
+#
+#                 # Total of every product in cart is grant_total
+#                 cart_items = CartItem.objects.filter(customer=my_user).order_by('id')
+#
+#                 for item in cart_items:
+#                     grant_total += (item.product.product_price * item.quantity)
+#
+#             except CartItem.DoesNotExist:
+#                 pass
+#
+#             try:
+#                 # check out handling
+#                 checkout = Checkout.objects.get(user=my_user)
+#
+#                 checkout.total = grant_total
+#                 if grant_total > 2500:
+#                     delivery_charge = 0
+#                 checkout.shipping = delivery_charge  # checkout saving delivery charge
+#
+#                 tax = (grant_total * 3) // 100
+#                 checkout.tax = tax
+#                 total_amount = (grant_total + tax + delivery_charge)  # calculating grand total.
+#                 checkout.grand_total = total_amount
+#
+#                 checkout.save()
+#             except Checkout.DoesNotExist:
+#                 pass
+#
+#         else:
+#             cart = Cart.objects.get(
+#                 cart_id=_cart_id(request))  # fetch the current cart, cart will be present in this case.
+#             try:
+#                 cart_item = CartItem.objects.get(product=variant, cart=cart)  # fetch the cart item
+#                 if cart_item.quantity > 1:
+#                     cart_item.quantity = cart_item.quantity - 1
+#                     cart_item.save()
+#                 else:
+#                     cart_item.delete()
+#                 product_quantity = cart_item.quantity
+#             except CartItem.DoesNotExist:
+#                 pass
+#
+#             return redirect('cart')
+#
+#         return JsonResponse({
+#             'quantity': cart_item.quantity,
+#             'total': total,
+#             'delivery_charge': delivery_charge,
+#             'tax': tax,
+#             'grant_total': grant_total,
+#             'total_amount': checkout.grand_total,
+#         })
+#     else:
+#         return redirect('cart')
+
+
+@cache_control(no_cache=True, no_store=True)
+def add_cart_quandity(request):
+    if request.method == 'POST':
+        grant_total, total_amount, total, tax = 0, 0, 0, 0
+        delivery_charge = 99
+
+        variant_id = request.POST.get('product_id')
+        product_quantity = request.POST.get('product_quandity')
+        product_stock = request.POST.get('productStock')
+
+        variant = ProductVariant.objects.get(id=variant_id)
+
+        if 'user' in request.session:
+            my_user = request.user
+            print("---------------------------")
+            print(my_user)
+            try:
+                cart_item = CartItem.objects.get(customer=my_user, product=variant)
+                print("---------------------------")
+                print(variant)
+                if variant.stock > cart_item.quantity:
+                    cart_item.quantity += 1
+
+                cart_item.save()
+
+                product_quantity = cart_item.quantity
+                total = cart_item.product.product_price * cart_item.quantity
+                cart_items = CartItem.objects.filter(customer=my_user)
+
+                for item in cart_items:
+                    grant_total += (item.product.product_price * item.quantity)
+
+            except CartItem.DoesNotExist:
+                pass
+
+            try:
+                # check out handling
+                checkout = Checkout.objects.get(user=my_user.id)
+
+                checkout.total = grant_total
+                if grant_total > 2500:
+                    delivery_charge = 0
+                checkout.shipping = delivery_charge  # checkout saving delivery charge
+                tax = (grant_total * 3) // 100  # tax logic
+                checkout.tax = tax
+                total_amount = (grant_total + tax + delivery_charge)  # calculating grand total.
+                checkout.grand_total = total_amount
+
+                checkout.save()
+
+            except Checkout.DoesNotExist:
+                pass
+
+        else:
+
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                cart_item = CartItem.objects.get(cart=cart, product=variant)
+                if variant.stock > cart_item.quantity:
+                    cart_item.quantity += 1
+                    product_quantity += 1
+                else:
+                    messages.error(request, "No more products available in the current variant")
+                cart_item.save()
+                total = cart_item.product.product_price * cart_item.quantity
+
+            except CartItem.DoesNotExist:
+                pass
+
+        return JsonResponse({
+            'quantity': product_quantity,
+            'total': total,
+            'grant_total': grant_total,
+            'tax': tax,
+            'total_amount': total_amount,
+            'delivery_charge': delivery_charge,
+            'product_stock': product_stock,
         })
     else:
         return redirect('cart')
--- static/js/update_cart.js
+++ static/js/update_cart.js
@@ -1,8 +1,9 @@
-$('.increseQuantity').click(function (e){
-    e.preventDefault()
+$('.increaseQuantity').click(function (e){
+    console.log("working")
 
     let product_id = $(this).closest('.product_data').find('.product_id').val()
     let product_qty = $(this).closest('.product_data').find('.inputQuantity').val()
+    let productStock = $(this).closest('.product_data').find('.product_stock').val()
     let token = $('input[name="csrfmiddlewaretoken"]').val()
 
     $.ajax({
@@ -11,11 +12,14 @@ $('.increseQuantity').click(function (e){
         data: {
             'product_id': product_id,
             'product_quandity': product_qty,
+            'productStock': productStock,
             csrfmiddlewaretoken: token,
         },
         
         success:(res)=>{
+            console.log(res.total_amount)
             console.log('increase');
+
             document.getElementById(product_id+"-qty").value=res.quantity
             document.getElementById(product_id+"-qtyL").innerHTML=res.quantity
             document.getElementById(product_id+"-total").innerHTML=res.total
@@ -23,19 +27,22 @@ $('.increseQuantity').click(function (e){
             document.getElementById('cartTotal').innerHTML=res.total_amount
             document.getElementById('tax').innerHTML=res.tax
             document.getElementById('deliveryCharge').innerHTML=res.delivery_charge
-            window.location.reload();
+            // window.location.reload();
+
 
         }
     })
 })
 
 
-
 $('.decreaseQuantity').click(function (e){
     e.preventDefault()
-    
+    console.log("working")
+
     let product_id = $(this).closest('.product_data').find('.product_id').val()
     let product_qty = $(this).closest('.product_data').find('.inputQuantity').val()
+    console.log(product_qty)
+    let product_stock = $(this).closest('.product_data').find('.product_stock').val()
     let token = $('input[name="csrfmiddlewaretoken"]').val()
 
     $.ajax({
@@ -44,10 +51,19 @@ $('.decreaseQuantity').click(function (e){
         data: {
             'product_id': product_id,
             'product_quandity': product_qty,
+            'product_stock': product_stock,
             csrfmiddlewaretoken: token,
         },
-        success:(res)=>{
+        success:(res)=> {
             console.log('decrease');
+            const stockOver = document.getElementById("stockOver")
+            let product_quantity = res.quantity
+            let stock = res.product_stock
+            if (product_quantity === stock) {
+                stockOver.innerHTML = "no more product available"
+            }
+            stockOver.innerHTML = ""
+
             document.getElementById(product_id+"-qty").value=res.quantity
             document.getElementById(product_id+"-qtyL").innerHTML=res.quantity
             document.getElementById(product_id+"-total").innerHTML=res.total
@@ -55,7 +71,8 @@ $('.decreaseQuantity').click(function (e){
             document.getElementById('cartTotal').innerHTML=res.total_amount
             document.getElementById('tax').innerHTML=res.tax
             document.getElementById('deliveryCharge').innerHTML=res.delivery_charge
-            window.location.reload();
+            // window.location.reload();
+
         }
     })
 })
