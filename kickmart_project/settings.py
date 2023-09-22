25b7c97ccd9c66fd0694ebb2a42e97532b078f5f
adish-ct <adishathu525@gmail.com>
2023-09-22 10:40:57 +0530

    requirement txt added

--- kickmart_project/settings.py
+++ kickmart_project/settings.py
@@ -13,7 +13,6 @@ import os
 from pathlib import Path
 from django.contrib.messages import constants as messages
 from decouple import config
-from cryptography.fernet import Fernet
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
