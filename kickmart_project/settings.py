c8751c0455f53bade3de414602a29c68ca39dfb7
adish-ct <adishathu525@gmail.com>
2023-09-23 23:13:35 +0530

    settings privacy updated

--- kickmart_project/settings.py
+++ kickmart_project/settings.py
@@ -155,7 +155,8 @@ MESSAGE_TAGS = {
 
 # otp handling
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+EMAIL_USE_TLS = True
 EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
