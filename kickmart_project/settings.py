dcc5697c405bdc297ee20693f5e948d435d7c174
adish-ct <adishathu525@gmail.com>
2023-09-24 11:15:21 +0530

    settings privacy updated

--- kickmart_project/settings.py
+++ kickmart_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
